308fb5898f7629572523869b4cf2ff5558715055
Amaliaazizah20 <97652559+Amaliaazizah20@users.noreply.github.com>
2022-01-13 13:26:31 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    //masukan 2 nilai awal 
+
+    $angka_pertama=0;
+    $angka_kedua=1;
+    $angka_ketiga=1;
+
+    // menampilkan 2 angka pertama 
+    echo $angka_pertama ."<br>". $angka_kedua. "<br>".$angka_ketiga;
+    for ($i=0; $i<18; $i++){
+        //menghtng angka yang ditampilkan 
+        $hasil = $angka_pertama + $angka_kedua + $angka_ketiga;
+        echo "<br>".$hasil;
+
+        $angka_pertama =$angka_kedua;
+        $angka_kedua = $angka_ketiga;
+        $angka_ketiga =$hasil;
+
+    }
